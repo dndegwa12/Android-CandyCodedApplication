83171be67d7fc570d558f680af3c6d5ac9f5cb22
Dickson <dicksonndegwa2@gmail.com>
2019-11-01 18:02:43 +0300

    First Commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -42,4 +42,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("+254720660513"));
+        startActivity(phoneIntent);
+    }
 }
